a512368723ce8cfd01d8c453caed5169bd2a8652
Dawid Wojtaszek <dawidw22@gmail.com>
2024-01-30 13:46:47 +0100

    remove unused imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import { FirebaseProvider } from "./context/firebase";
-import { UserDataProvider } from "./context/userData";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,12 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <FirebaseProvider>
-        <UserDataProvider>
-          <body className={`bg-slate-800 text-white ${inter.className}`}>
-            <Header />
-            {children}
-          </body>
-        </UserDataProvider>
+        <body className={`bg-slate-800 text-white ${inter.className}`}>
+          <Header />
+          {children}
+        </body>
       </FirebaseProvider>
     </html>
   );
