950c08b6432b72967bf108e67b45ec49bae10f16
Dawid Wojtaszek <dawidw22@gmail.com>
2024-02-08 12:16:36 +0100

    add delete option and show project type in todo item

--- src/app/components/task-item.jsx
+++ src/app/components/task-item.jsx
@@ -1,7 +1,40 @@
+"use client";
+import { useFirebaseContext } from "../context/firebase";
+
 const TaskItem = ({ task }) => {
+  console.log("render");
+  const {
+    setLocalTasks,
+    updateLocalUserData,
+    setUserDataInStorage,
+    currentUser,
+    userData,
+  } = useFirebaseContext();
+  const handleDelete = async (e) => {
+    e.preventDefault();
+
+    const newTaskList = userData.tasks.filter((e) => e.id != task.id);
+    console.log(newTaskList);
+    const newUserData = {
+      projects: [...userData.projects],
+      tasks: newTaskList,
+    };
+    await setUserDataInStorage(newUserData, currentUser.uid);
+    await updateLocalUserData();
+    setLocalTasks(newTaskList);
+  };
   return (
-    <div className=" p-4 min-h-[60px] mb-3 border-white rounded-md border">
-      {task.name}
+    <div className=" p-4 min-h-[60px] mb-3 border-white rounded-md border flex justify-between">
+      <div className="flex items-center">
+        <span>{task.name}</span>
+        <div className=" bg-lime-300 text-black rounded-md text-xs ml-4 ">
+          {task.project}
+        </div>
+      </div>
+
+      <button className=" bg-red-400 rounded-md px-3" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 };
--- src/app/components/task-list.jsx
+++ src/app/components/task-list.jsx
@@ -3,7 +3,6 @@ import AddTask from "./add-task";
 import { useFirebaseContext } from "../context/firebase";
 const TaskList = () => {
   const { localTasks } = useFirebaseContext();
-
   return (
     <div className="flex w-full bg-slate-600 flex-col p-4">
       <AddTask />
