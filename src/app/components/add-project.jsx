30c31149aaf969850a0b79ba1ea643a132c54608
Dawid Wojtaszek <dawidw22@gmail.com>
2024-02-14 15:04:58 +0100

    create add project button

---
+++ src/app/components/add-project.jsx
@@ -0,0 +1,11 @@
+const AddProject = () => {
+  return (
+    <div className="w-full flex justify-end ">
+      <button className=" rounded-full font-bold bg-slate-500 w-8 h-8 flex justify-center items-center ">
+        +
+      </button>
+    </div>
+  );
+};
+
+export default AddProject;
--- src/app/components/projects-list.jsx
+++ src/app/components/projects-list.jsx
@@ -1,15 +1,19 @@
 "use client";
 import ProjectItem from "./project-item";
+import AddProject from "./add-project";
 import { useFirebaseContext } from "../context/firebase";
 
 const ProjectList = () => {
   const { userData } = useFirebaseContext();
   return (
-    <div className="w-[250px] bg-slate-600 flex flex-col">
-      Projects list:
-      {userData.projects.map((e, index) => (
-        <ProjectItem project={e} key={index} />
-      ))}
+    <div className="w-[250px] bg-slate-600 flex flex-col p-3">
+      <span className=" font-bold uppercase mb-3">Projects list:</span>
+      <div className="flex flex-col">
+        {userData.projects.map((e, index) => (
+          <ProjectItem project={e} key={index} />
+        ))}
+      </div>
+      <AddProject />
     </div>
   );
 };
